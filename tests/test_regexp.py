36a964e8d79e8c85c20c7c4e391c1d076dbac16d
olol85 <olol85@gmail.com>
2023-08-13 12:01:48 +0200

    renames

--- tests/test_regexp.py
+++ tests/test_regexp.py
@@ -1,11 +1,11 @@
 from app.worker import _init_and_upload
 
 
-def test_regexp_leaves_no_items(app, mock_file_tree):
+def test_regexp_no_match(app, mock_file_tree):
 
     from app.models import Item
 
-    job = _init_and_upload(
+    _init_and_upload(
         "file://root/path/project/",
         "s3://bucket/project/",
         regexp="^.*\.funnyextension",
@@ -18,7 +18,7 @@ def test_regexp_that_match(app, mock_file_tree):
 
     from app.models import Item
 
-    job = _init_and_upload(
+    _init_and_upload(
         "file:///root/path/project/", "s3://bucket/project/", regexp="^.*\.ext"
     )
     items = Item.query.all()
