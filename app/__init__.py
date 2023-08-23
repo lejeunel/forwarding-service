a283660b4cc34f58384592e84bb3cd47ec095279
olol85 <olol85@gmail.com>
2023-08-23 20:01:55 +0200

    readme

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from decouple import config
-from flask_sqlalchemy.model import DefaultMeta
 from sqlalchemy.orm import sessionmaker
+from pathlib import Path
+from sqlalchemy import create_engine
 
 from app.auth import S3StaticCredentials
 from app.file import FileSystemReader
@@ -8,21 +9,28 @@ from app.models import Base
 from app.s3 import S3Writer
 from app.transfer_agent import TransferAgent
 
+
 def setup_db():
-    from sqlalchemy import create_engine
-    engine = create_engine("sqlite:///uploads.db")
+    db_path = Path(
+        config("FORW_SERV_DB_PATH", "~/.cache/forwarding_service.db")
+    ).expanduser()
+    assert db_path.parent.exists(), f"{db_path.parent} not found."
+
+    engine = create_engine(f"sqlite:///{db_path}")
     Session = sessionmaker(bind=engine)
     session = Session()
     Base.metadata.create_all(engine)
 
     return session
 
-def make_agent():
 
+def make_agent():
     session = setup_db()
 
-    auth = S3StaticCredentials(aws_access_key_id=config('AWS_ACCESS_KEY_ID'),
-                               aws_secret_access_key=config('AWS_SECRET_ACCESS_KEY'))
+    auth = S3StaticCredentials(
+        aws_access_key_id=config("AWS_ACCESS_KEY_ID", None),
+        aws_secret_access_key=config("AWS_SECRET_ACCESS_KEY", None),
+    )
     writer = S3Writer(auth)
     agent = TransferAgent(session, reader=FileSystemReader(), writer=writer)
 
